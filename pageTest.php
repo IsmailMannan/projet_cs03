db19367b46057df5e792e4e3a078c44d24985f12
IsmailMannan <ismail.mannan@utt.fr>
2017-12-03 21:46:37 +0100

    Derniere version

--- pageTest.php
+++ pageTest.php
@@ -1,5 +1,17 @@
 <?php
-// Sous WAMP (Windows)
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Premiers tests du CSS</title>
+    </head>
+
+    <body>
+        <h1>Mon super site</h1>
+        
+        <p>Bonjour et bienvenue sur mon site !</p>
+        <p>Pour le moment, mon site est un peu <em>vide</em>. Patientez encore un peu !</p>
+    </body>
 $bdd = new PDO('mysql:host=localhost;dbname=bdh;charset=utf8', 'root', '');
 
 $reponse = $bdd->query('Select * from test');
